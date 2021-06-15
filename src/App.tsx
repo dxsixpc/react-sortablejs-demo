576780101e0c0d8837668a6ed991be66e8cfcc8f
zhupengcheng <1071667708@qq.com>
2021-06-15 16:57:30 +0800

    feat: 加密测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
-import * as React from "react";
-import { useState } from "react";
-import { Layout } from "antd";
-import "antd/dist/antd.css";
-const { Header, Footer, Sider, Content } = Layout;
+import * as React from 'react'
+import { useState } from 'react'
+import { Layout } from 'antd'
+import 'antd/dist/antd.css'
+const { Header, Footer, Sider, Content } = Layout
 
-import ComponentList from "./ComponentList/ComponentList";
-import Canvas from "./Canvas/Canvas";
-import Dustbin from "./Dustbin/Dustbin";
+import { CrypterAES } from './Crypter/CrypterAES'
+import { data, key } from './Crypter/config'
+
+import ComponentList from './ComponentList/ComponentList'
+import Canvas from './Canvas/Canvas'
+import Dustbin from './Dustbin/Dustbin'
 
 const App = () => {
-  const [data, setData] = useState([]); //数据树
-  const LcdpData = {
-    id: "page1",
-    type: "LcdpForm",
-    props: {
-      mode: "inline",
-    },
-    children: data,
-  };
+  // const [data, setData] = useState([]); //数据树
+  // const LcdpData = {
+  //   id: "page1",
+  //   type: "LcdpForm",
+  //   props: {
+  //     mode: "inline",
+  //   },
+  //   children: data,
+  // };
 
-  const changeData = (dataList: any) => {
-    setData([...dataList]);
-    // console.log('总数据', LcdpData);
-  };
+  // const changeData = (dataList: any) => {
+  //   setData([...dataList]);
+  //   // console.log('总数据', LcdpData);
+  // };
+
+  console.log(99090)
+
+  const encryptdata = data
+  const nonceStr = key
+
+  const dbCrypter = new CrypterAES()
+  // 设置秘钥
+  // * 截取nonceStr的前十六位
+  var padEnd = Array(16).fill(' ').join('')
+  let keyStr = ('' + nonceStr + padEnd).slice(0, 16)
+
+  console.log(keyStr)
+  dbCrypter.updateKey(keyStr)
+  // 加密
+  // const dataEncrypted = dbCrypter.encrypt('12345678890');
+  // console.log(dataEncrypted)
+  // 解密
+  const value = dbCrypter.decrypt(encryptdata,'dataType')
+
+  console.log(value)
 
   return (
     <>
-      <Layout>
+      {/* <Layout>
         <Header>Header</Header>
         <Layout>
           <Sider>
@@ -40,9 +64,9 @@ const App = () => {
           </Sider>
         </Layout>
         <Footer>Footer</Footer>
-      </Layout>
+      </Layout> */}
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
